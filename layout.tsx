2bef3a5b322de182ae16ae47497f3c68285e2aa6
S.watana <75829584+opepelab@users.noreply.github.com>
2021-07-26 19:55:44 +0900

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -3,7 +3,7 @@ import Footer from './Footer'
 import Navbar from './Navbar'
 import PictureHigh from './PictureHigh'
 // すべての共通して使うコンポーネントはこのファイルを介して作動する starting components this file.
-const Layout = ({ children }) => {
+const Layout: React.FC = ({ children }) => {
     return (
       <div className="resp">
         <Metatxt />
